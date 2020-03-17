aeb11506419fda55742be4ef11c998abd6a2492d
Mizok <neux@Neux99de-MBP.Home>
2020-03-17 23:25:13 +0800

    暫時移除firebase

--- dist/js/customize.js
+++ dist/js/customize.js
@@ -236,19 +236,19 @@ var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userA
 //     }
 // }
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCdrjun_PkAxv_GWo_S8KUTdjx6M8G1z74",
-    authDomain: "lottie-player.firebaseapp.com",
-    databaseURL: "https://lottie-player.firebaseio.com",
-    projectId: "lottie-player",
-    storageBucket: "lottie-player.appspot.com",
-    messagingSenderId: "792201423394",
-    appId: "1:792201423394:web:b8d76175cc2a856778b87b",
-    measurementId: "G-T221N4QJTM"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCdrjun_PkAxv_GWo_S8KUTdjx6M8G1z74",
+//     authDomain: "lottie-player.firebaseapp.com",
+//     databaseURL: "https://lottie-player.firebaseio.com",
+//     projectId: "lottie-player",
+//     storageBucket: "lottie-player.appspot.com",
+//     messagingSenderId: "792201423394",
+//     appId: "1:792201423394:web:b8d76175cc2a856778b87b",
+//     measurementId: "G-T221N4QJTM"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
 
 
 var lottieObj = {
